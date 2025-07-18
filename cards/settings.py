a389155c86ef1dc18d288e50a8455b99c2c291d7
theertha09 <161118217+theertha09@users.noreply.github.com>
2025-07-18 16:28:01 +0530

    ;

--- cards/settings.py
+++ cards/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -107,7 +107,6 @@ AUTH_PASSWORD_VALIDATORS = [
 CORS_ALLOWED_ORIGINS = [
     "https://yourfrontend.com",
     "http://localhost:3000",
-    "http://localhost:5173/",
     
 ]
 
@@ -132,3 +131,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- cards/urls.py
+++ cards/urls.py
@@ -16,13 +16,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
-
     path('admin/', admin.site.urls),
     path('api/', include('user.urls')),  # Include user app URLs
-      # Include refer app URLs
     path('api/', include('form.urls')),  # Include form app URLs
-
 ]
+
+# ✅ Serve media files in development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
